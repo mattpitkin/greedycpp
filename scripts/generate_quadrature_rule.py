80c8b26ded024c26e5750d44e8ba1b6eacbdeee8
Scott Field <sfield@umd.edu>
2014-09-30 14:57:41 -0400

    generate multidomain gaussian quadrature rule by python script

---
+++ scripts/generate_quadrature_rule.py
@@ -0,0 +1,48 @@
+''' generate a mutlidomain quadrature rule for input into the greedy alg '''
+
+import numpy as np
+from scipy.special.orthogonal import p_roots
+
+
+### setup quadrature rule ###
+pts = 6
+dom_ints = [10.0,30.0,1024.0]
+#dom_ints = [10.0,1024.0]
+
+def SingleDomGaussLeg(quad_points,a,b):
+    '''a,b domain endpoints. quad_points is number of quadrature points'''
+
+    [x,w] = p_roots(quad_points)
+    x = x.real
+    y = (b-a)*(x+1.0)/2.0 + a
+    w = (b-a)/2.0*w
+    return y,w
+
+def MultiDomain(quad_points,dom_intervals):
+    '''[x0,x1],[x1,x2]... are the domain intervals'''
+
+    for i in range(1,len(dom_intervals)):
+      if ( dom_intervals[i] <= dom_intervals[i-1]): 
+          raise Exception,"boundaries of intervals must be monotonic."
+
+    x = np.zeros((0,0),float)
+    w = np.zeros((0,0),float)
+    for i in range(len(dom_intervals)-1):
+        x_dom,w_dom = SingleDomGaussLeg(quad_points,dom_intervals[i],dom_intervals[i+1])
+        x = np.append(x,x_dom)
+        w = np.append(w,w_dom)
+
+    return x,w
+
+quad_nodes,quad_weights = MultiDomain(pts,dom_ints)
+
+fp = open('MyQuadPoints.txt','w')
+fw = open('MyQuadWeights.txt','w')
+for ii in range(len(quad_nodes)):
+    fp.write('%1.15e\n' % quad_nodes[ii])
+    fw.write('%1.15e\n' % quad_weights[ii])
+
+fp.close()
+fw.close()
+
+
