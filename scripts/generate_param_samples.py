535760cb7a1a1ff4738deb47e17ef98147f47323
Scott Field <sfield@umd.edu>
2014-10-02 17:35:27 -0400

    generate random samples from python script

--- scripts/generate_param_samples.py
+++ scripts/generate_param_samples.py
@@ -5,53 +5,76 @@
 import numpy as np
 import math
 
+### output file name ###
+filename = 'RandomSamples.txt'
+
 ### sampling strategies (linear, log10, ln) ###
 param_sampling = "ln";
 scaling_factor = 20.0 # default should be 1. higher values densely sample lower range of parameter interval
+sample_type    = "rand" # "rand" or "deterministc"
 
 ### setup parameter intervals ###
-params_num  = [500,500]  # params_num[i] is the number of samplings in the interval [param_low[i],param_high[i]]
+params_num  = [50,50]  # params_num[i] is the number of samplings in the interval [param_low[i],param_high[i]]
 params_low  = [1.0,1.0]  # lower interval of each parameter
-params_high = [4.0,4.0]  # upper interval of each parameter
+params_high = [3.0,3.0]  # upper interval of each parameter
 
+if( sample_type is "deterministic"):
 
-### from franks code ###
-def rescale_mass(x,p_min,p_max):
-  "rescale the mass range to be in [m_min,m_max] if upper log-range has been pushed out"
-  a,b0,b = p_min,p_max,p_max*scaling_factor
-  c0,c1=a*(-b + b0)/(a - b),(a - b0)/(a - b)
-  return c0+c1*x
+  print "deterministic sampling"
 
-p_low  = params_low[0]
-p_high = params_high[0]
-p_num  = params_num[0]
-if(param_sampling=="linear"):
-  p1 = np.linspace(p_low,p_high,p_num,endpoint=True)
-if(param_sampling=="ln"):
-  mass_params_unscaled = np.logspace(math.log(p_low), math.log(scaling_factor*p_high), p_num, endpoint=True, base=math.exp(1))
-  mass_params=rescale_mass(mass_params_unscaled, p_low, p_high)
-  p1 = mass_params
-if(param_sampling=="log10"):
-  mass_params_unscaled = np.logspace(math.log10(p_low), math.log10(scaling_factor*p_high), p_num, endpoint=True, base=10.0)
-  mass_params=rescale_mass(mass_params_unscaled)
-  p1 = mass_params
+  ### from franks code ###
+  def rescale_mass(x,p_min,p_max):
+    "rescale the mass range to be in [m_min,m_max] if upper log-range has been pushed out"
+    a,b0,b = p_min,p_max,p_max*scaling_factor
+    c0,c1=a*(-b + b0)/(a - b),(a - b0)/(a - b)
+    return c0+c1*x
 
-### this could be more explict use of log spacing ###
-#p1 = np.power(params_low[0]*(params_high[0]/params_low[0]),np.linspace(0,1,params_num[0]))
-#p2 = np.power(params_low[1]*(params_high[1]/params_low[1]),np.linspace(0,1,params_num[1]))
+  p_low  = params_low[0]
+  p_high = params_high[0]
+  p_num  = params_num[0]
+  if(param_sampling=="linear"):
+    p1 = np.linspace(p_low,p_high,p_num,endpoint=True)
+  if(param_sampling=="ln"):
+    mass_params_unscaled = np.logspace(math.log(p_low), math.log(scaling_factor*p_high), p_num, endpoint=True, base=math.exp(1))
+    mass_params=rescale_mass(mass_params_unscaled, p_low, p_high)
+    p1 = mass_params
+  if(param_sampling=="log10"):
+    mass_params_unscaled = np.logspace(math.log10(p_low), math.log10(scaling_factor*p_high), p_num, endpoint=True, base=10.0)
+    mass_params=rescale_mass(mass_params_unscaled)
+    p1 = mass_params
 
-p2 = p1
+  ### this could be more explict use of log spacing ###
+  #p1 = np.power(params_low[0]*(params_high[0]/params_low[0]),np.linspace(0,1,params_num[0]))
+  #p2 = np.power(params_low[1]*(params_high[1]/params_low[1]),np.linspace(0,1,params_num[1]))
 
+  p2 = p1
 
-### sanity checks ###
-if p1.min()<p_low: raise Exception("samples too low!")
-if p1.max()>p_high: raise Exception("samples too high!")
-for i in range(1,len(p1)):
-    if p1[i]-p1[i-1]<=0: raise Exception("samples not monotonic!")
+  ### sanity checks ###
+  if p1.min()<p_low: raise Exception("samples too low!")
+  if p1.max()>p_high: raise Exception("samples too high!")
+  for i in range(1,len(p1)):
+      if p1[i]-p1[i-1]<=0: raise Exception("samples not monotonic!")
+
+elif( sample_type is "rand"):
+
+  p_low  = params_low[0]
+  p_high = params_high[0]
+  p_num  = params_num[0]
+  p1 = (p_high - p_low) * np.random.random_sample((p_num,)) + p_low
+
+  p_low  = params_low[1]
+  p_high = params_high[1]
+  p_num  = params_num[1]
+  p2 = (p_high - p_low) * np.random.random_sample((p_num,)) + p_low
+  
+  print "random sampling.\nWARNING: due to tensor product structure this is not uniformly drawn from (p1,p2) space."
+
+else:
+  raise Exception("sampling type unknown")
 
 
 ### output MyTS.txt here -- tensor product parameter grid ###
-fp = open('MyTS.txt','w')
+fp = open(filename,'w')
 for ii in range(np.size(p1)):
   for jj in range(np.size(p2)):
     fp.write('%1.15e\t' % p1[ii])
