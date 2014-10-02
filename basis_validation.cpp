a283efa32586463db29fcbfbfc738d8dd66ec621
Scott Field <sfield@umd.edu>
2014-10-02 15:37:50 -0400

    finishing up basis validation routine

--- basis_validation.cpp
+++ basis_validation.cpp
@@ -5,7 +5,6 @@
 //
 // PURPOSE: test basis accuracy from random model samples
 
-//#include <iomanip>
 #include <iostream>
 #include <fstream>
 #include <stdio.h>
@@ -26,6 +25,9 @@
 
 #include "parameters.hpp"
 #include "gsl_helper_functions.h"
+#include "quadratures.h"
+#include "training_set.hpp"
+#include "my_models.h"
 
 int main (int argc, char **argv) {
 
@@ -43,30 +45,67 @@ int main (int argc, char **argv) {
   Parameters *params_from_file = new Parameters(argv);
   std::cout << *params_from_file;
 
-  gsl_matrix_complex *RB_space;
-  RB_space = gsl_matrix_complex_alloc(params_from_file->max_RB(),params_from_file->quad_points());
+  gsl_matrix_complex *RB_space, *model_evaluations;
+  gsl_vector_complex *wQuad, *model_eval, *r_tmp;
+  gsl_vector *xQuad;
+  double * errors;
+  char err_filename[100];
+  FILE *err_data;
 
-  // create basis verifcation folder as subfolder of output
+  model_eval = gsl_vector_complex_alloc(params_from_file->quad_points());  
+  r_tmp      = gsl_vector_complex_alloc(params_from_file->max_RB());
+  RB_space   = gsl_matrix_complex_alloc(params_from_file->max_RB(),params_from_file->quad_points());
 
-  // read in basis and output to file
+  // read in basis from gsl binary file //
   char rb_filename[100];
   FILE *pBASIS;
   strcpy(rb_filename,argv[2]);
   strcat(rb_filename,"/Basis.bin");
-
   std::cout << "reading basis from file " << rb_filename << " ..." << std::endl;
-
   pBASIS = fopen(rb_filename,"rb");
   gsl_matrix_complex_fread(pBASIS,RB_space);
   fclose(pBASIS);
-
   //char testwrite[100];
   //strcpy(testwrite,"RB_test");
   //mygsl::gsl_matrix_complex_fprintf(testwrite,RB_space);
 
+  // reconstruct quadrature rule used in greedy //
+  SetupQuadratureRule(&wQuad,&xQuad,params_from_file);
+
+  // TODO: overload so random points can be filled here //
+  TrainingSetClass *random_samples = new TrainingSetClass(params_from_file,1);
+
+  model_evaluations = gsl_matrix_complex_alloc(random_samples->ts_size(),xQuad->size);
+  errors            = new double[random_samples->ts_size()];
+
+  mymodel::FillTrainingSet(model_evaluations,xQuad,wQuad,*random_samples,0);
+
+  // TODO: use openMP for this part //
+  // error reported will be \sqrt(h - Ph) //
+  for(int ii = 0; ii < random_samples->ts_size(); ii++) {
+    gsl_matrix_complex_get_row(model_eval,model_evaluations,ii);
+    mygsl::MGS(r_tmp,model_eval,RB_space,wQuad,params_from_file->max_RB()-1);
+    errors[ii] = GSL_REAL(gsl_vector_complex_get(r_tmp,params_from_file->max_RB()-1));
+  }
+
+
+  strcpy(err_filename,argv[2]);
+  strcat(err_filename,"/VerErrors.txt");
+  err_data = fopen(err_filename,"w");
+  for(int i = 0; i < random_samples->ts_size() ; i++) {
+    fprintf(err_data,"%1.15e\n",errors[i]);
+  }
+  fclose(err_data);
 
 
   gsl_matrix_complex_free(RB_space);
+  gsl_matrix_complex_free(model_evaluations);
+  gsl_vector_complex_free(r_tmp);
+  gsl_vector_complex_free(model_eval);
+  gsl_vector_complex_free(wQuad);
+  gsl_vector_free(xQuad);
+
+  delete [] errors;
 
   delete params_from_file;
   params_from_file = NULL;
