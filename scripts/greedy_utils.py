4a6098b4b04a94ace3ebf7251e3310ded3fc5d03
Scott Field <sfield@umd.edu>
2015-05-11 10:30:08 -0400

    python tests and utilities build equally spaced quadrature rules. some reorganization of common files

--- scripts/greedy_utils.py
+++ scripts/greedy_utils.py
@@ -5,29 +5,34 @@ import numpy as np
 
 def load_ts(outdir):
 
-    TS_real = np.loadtxt(outdir+'/TSpace_real.txt')
-    TS_imag = np.loadtxt(outdir+'/TSpace_imag.txt')
-    evaluations, quad_points = TS_real.shape
-    TS = np.zeros((quad_points,evaluations), dtype=np.complex)
-    TS[:,:].real = TS_real.transpose()
-    TS[:,:].imag = TS_imag.transpose()
+  TS_real = np.loadtxt(outdir+'/TSpace_real.txt')
+  TS_imag = np.loadtxt(outdir+'/TSpace_imag.txt')
+  evaluations, quad_points = TS_real.shape
+  TS = np.zeros((quad_points,evaluations), dtype=np.complex)
+  TS[:,:].real = TS_real.transpose()
+  TS[:,:].imag = TS_imag.transpose()
 
-    return TS
+  return TS
 
-def load_quad(outdir):
+def load_quad(outdir=None,xmin=None,xmax=None,quad_points=None):
+  ''' if outdir is supplied, the quadrature rule is loaded.
+      otherwise x=np.linspace(xmin,xmax,quad_points) is used'''
+
+  if(outdir is not None):
     quad_rule  = np.loadtxt(outdir+'/quad_rule.txt')
     weights    = quad_rule[:,1]
     nodes      = quad_rule[:,0]
-    return weights,nodes
+  else:
+    nodes   = np.linspace(xmin,xmax,quad_points)
+    weights = (nodes[1]-nodes[0])*np.ones(nodes.shape)
+  return weights, nodes
 
-def load_info(outdir):
+def load_basis(outdir):
 
-    ### load the basis and quadrature weights ###
+  ### load the basis and quadrature weights ###
+  try: 
     basis_real = np.loadtxt(outdir+'/Basis_real.txt')
     basis_imag = np.loadtxt(outdir+'/Basis_imag.txt')
-    quad_rule  = np.loadtxt(outdir+'/quad_rule.txt')
-    weights    = quad_rule[:,1]
-    nodes      = quad_rule[:,0]
 
     ### remove the zeros --- final XXX rows could be identically zero ###
     basis_real = basis_real[np.where(np.any(basis_real != 0, axis=1))]
@@ -39,4 +44,26 @@ def load_info(outdir):
     B[:,:].real = basis_real.transpose()
     B[:,:].imag = basis_imag.transpose()
 
-    return B, weights, nodes
+    print 'Found basis in a text file'
+
+    return B
+  except IOError: 
+    try:
+      B = np.load(outdir+'/Basis.npy')
+      B = B.transpose()
+      print 'Found basis in a numpy file'
+      return B
+    except IOError:
+      print 'No valid basis files could be found'
+
+def load_info(outdir,xmin=None,xmax=None,quad_points=None):
+  ''' if outdir is supplied, the quadrature rule is loaded.
+      otherwise x=np.linspace(xmin,xmax,quad_points) is used'''
+
+  B = load_basis(outdir)
+  if(xmax is not None):
+    weights, nodes = load_quad(None,xmin,xmax,quad_points)
+  else:
+    weights, nodes = load_quad(outdir)
+
+  return B, weights, nodes
--- scripts/sanity_tests.py
+++ scripts/sanity_tests.py
@@ -11,73 +11,41 @@ Can run at command line from an application directory, e.g.:
 
 import os, re, sys
 import numpy as np
+import greedy_utils
 
-def load_info(outdir):
+def ortho_test(outdir,xmin=None,xmax=None,quad_points=None):
 
-    ### load the basis and quadrature weights ###
-    basis_real = np.loadtxt(outdir+'/Basis_real.txt')
-    basis_imag = np.loadtxt(outdir+'/Basis_imag.txt')
-    quad_rule  = np.loadtxt(outdir+'/quad_rule.txt')
-    weights    = quad_rule[:,1]
-    nodes      = quad_rule[:,0]
+  B, weights, nodes = greedy_utils.load_info(outdir,xmin,xmax,quad_points)
 
-    ### remove the zeros --- final XXX rows could be identically zero ###
-    basis_real = basis_real[np.where(np.any(basis_real != 0, axis=1))]
-    basis_imag = basis_imag[np.where(np.any(basis_imag != 0, axis=1))]
-
-    bases, quad_points = basis_real.shape
-    B = np.zeros((quad_points, bases), dtype=np.complex)
-
-    B[:,:].real = basis_real.transpose()
-    B[:,:].imag = basis_imag.transpose()
-
-    return B, weights, nodes
-
-def load_ts(outdir):
-
-    TS_real = np.loadtxt(outdir+'/TSpace_real.txt')
-    TS_imag = np.loadtxt(outdir+'/TSpace_imag.txt')
-    evaluations, quad_points = TS_real.shape
-    TS = np.zeros((quad_points,evaluations), dtype=np.complex)
-    TS[:,:].real = TS_real.transpose()
-    TS[:,:].imag = TS_imag.transpose()
-
-    return TS
-
-def ortho_test(outdir):
-
-    B, weights, nodes = load_info(outdir)
-
-    result = np.dot(weights * B.T.conj(),B) 
-
-    err = result - np.eye(result.shape[1])
-    print "othogonality error %1.15e" % np.max(np.abs(err))
+  result = np.dot(weights * B.T.conj(),B) 
+  err = result - np.eye(result.shape[1])
+  print "othogonality error %1.15e" % np.max(np.abs(err))
 
 def basis_accuracy_test(outdir):
 
-    B, weights, nodes = load_info(outdir)
+  B, weights, nodes = greedy_utils.load_info(outdir)
+  TS = greedy_utils.load_ts(outdir)
+  quad_points, evaluations = TS.real.shape 
 
-    TS = load_ts(outdir)
-    quad_points, evaluations = TS.real.shape 
 
-    err_app = np.zeros(evaluations)
+  err_app = np.zeros(evaluations)
 
-    # TODO: shouldn't use for-loop here
-    for ii in range(evaluations):
-        proj_coeff = np.dot(weights * B.T.conj(),TS[:,ii])
-        err_h = TS[:,ii] - np.dot(B,proj_coeff) # not conj is good! we just want the sum (no weights either)
-        err_app[ii] = np.sqrt( np.dot( weights * err_h,err_h.conj()) ).real
+  # TODO: shouldn't use for-loop here
+  for ii in range(evaluations):
+    proj_coeff = np.dot(weights * B.T.conj(),TS[:,ii])
+    err_h = TS[:,ii] - np.dot(B,proj_coeff) # not conj is good! we just want the sum (no weights either)
+    err_app[ii] = np.sqrt( np.dot( weights * err_h,err_h.conj()) ).real
 
-    import matplotlib.pyplot as plt
-    plt.semilogy(range(evaluations),err_app,'ro')
-    plt.show()
+  import matplotlib.pyplot as plt
+  plt.semilogy(range(evaluations),err_app,'ro')
+  plt.show()
 
-    ## to show other info ##
-    #import matplotlib.pyplot as plt
-    #plt.plot(nodes,TS[:,1])
-    #plt.show()
-    #plt.plot(nodes,err_h)
-    #plt.show()
+  ## to show other info ##
+  #import matplotlib.pyplot as plt
+  #plt.plot(nodes,TS[:,1])
+  #plt.show()
+  #plt.plot(nodes,err_h)
+  #plt.show()
 
 
 if __name__=="__main__":
