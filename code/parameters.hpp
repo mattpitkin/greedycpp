ca50f765f3317255fc45e66262652cde0b194080
Scott Field <sfield@astro.cornell.edu>
2015-09-25 11:17:04 -0700

    format compliance

--- code/parameters.hpp
+++ code/parameters.hpp
@@ -8,7 +8,7 @@
 
 class Parameters {
   public:
-    //Parameters();
+
     Parameters(char **,bool);
     ~Parameters();
 
@@ -32,7 +32,7 @@ class Parameters {
     inline std::string output_dir() const { return output_dir_; }
     inline std::string model_name() const { return model_name_; }
     inline std::string ts_file_name() const { return ts_file_name_; }
-    inline std::string output_data_format() const { return output_data_format_; }
+    inline std::string output_data_format() const { return output_data_format_;}
     inline std::string quad_nodes_file() const { return quad_nodes_file_; }
     inline std::string num_weight_file() const { return num_weight_file_; }
     inline std::string quad_weight_file() const { return quad_weight_file_; }
@@ -40,33 +40,96 @@ class Parameters {
   private:
     std::string config_file_name_; // name of cfg file. command line argument
 
-    // settings which MUST be set //
-    bool load_from_file_;          // load training points from file (if true, provide ts_file_name, if false parms low/high/num)
-    int quad_type_;                // 0 = Gaussian quadrature, 1 = Riemann, 2 = user-defined pointset (if 2, provide quad_nodes_file_, num_weight_file_)
-    bool weighted_inner_;          // whether to include a weight W(x): \int W(x) f(x) g(x)  (if true, provide weight_file_name)
+
+    //**** settings which MUST be set ****//
+
+    // load_from_file_: Load training points from file (If true, provide
+    // ts_file_name. If false, set parms_low, params_high and params_num)
+    bool load_from_file_;
+
+    // quad_type_: Gaussian quadrature (0), Riemann sum (1), user-defined
+    // pointset (2). If 2, provide quad_nodes_file_, num_weight_file_
+    int quad_type_;
+
+    // weighted_inner_: Whether to include a weight W(x), such that 
+    //  \int W(x) f(x) g(x)  (if true, provide weight_file_name)
+    bool weighted_inner_;
+
+    // seed_: Greedy algorithm seed
     int seed_;                     // greedy algorithm seed
-    double tol_;                   // greedy algorithm tolerance ( \| app \|^2)
-    int max_RB_;                   // estimated number of RB (reasonable upper bound)
-    int param_dim_;                // number of parametric dimensions
-    bool export_tspace_;           // if true, mpi_size must equal 1
-    double* param_scale_;          // scale each params[j][i] so that model evaluated at param_scale[i] * params[j][i]
 
-    // run settings - MAY need to be set //
-    double x_min_;                 // lower value x_min (physical domain) --> needed if quad_type != 2
-    double x_max_;                 // upper value x_max (physical domain) --> needed if quad_type != 2
-    int quad_points_;              // total number of quadrature points   --> needed if quad_type != 2
-    std::string quad_nodes_file_;  // file name for vector of quadrature points  --> needed if quad_type = 2
-    std::string num_weight_file_;  // file name of numerical weights             --> needed in quad_type = 2
-    std::string quad_weight_file_; // file name of weights --> needed if weighted_inner = true
+    // tol_: Greedy algorithm tolerance ( \| app \|^2)
+    double tol_;
 
-    // run settings - MAY need to be set //
-    double *params_low_, *params_high_; // this is required if load_from_file = false. lower/upper interval of each parameter 
-    int *params_num_;                   // this is required if load_from_file = false. Number of samplings in the interval [param_low,param_high]
-    std::string model_name_;            // name of model -- used to select the appropriate model in FillTrainingSet
-    std::string output_dir_;            // folder to put all output files
-    std::string output_data_format_;    // format of output files (text or gsl binary supported)
-    std::string ts_file_name_;          // this is required if load_from_file = true
-    bool ts_file_exists_;               // true if ts file loaded, otherwise false -- useful to avoid termination when file not found since a basis validation study might not make this available 
+    // max_RB_: Estimated number of RB (reasonable upper bound)
+    int max_RB_;
+
+    // param_dim_: Number of parametric dimensions
+    int param_dim_;
+
+    // export_tspace_: If true, mpi_size must equal 1
+    bool export_tspace_;
+
+    // TODO: should default to 1 
+    // param_scale_: scale each params[j][i] so that input to the model is 
+    // is the value (param_scale[i] * params[j][i])
+    double* param_scale_;
+
+    // Folder to put all output files
+    std::string output_dir_;
+
+    // Format of output files (text or gsl binary supported)
+    std::string output_data_format_;
+
+    // Name of model used to select the appropriate model in FillTrainingSet
+    std::string model_name_;
+
+
+    //**** run settings - MAY need to be set (quadrature info) ****//
+
+    // x_min_: Lower value x_min (physical domain). Set if quad_type != 2
+    double x_min_;
+
+    // x_min_: Upper value x_max (physical domain). Set if quad_type != 2
+    double x_max_;
+
+    // quad_points_: total number of quadrature points. Set if quad_type != 2
+    int quad_points_;
+
+    // quad_nodes_file_: File name for vector of quadrature points.
+    // Set if quad_type = 2
+    std::string quad_nodes_file_;
+
+    // num_weight_file_: File name of numerical (quadrature) weights.
+    // Set if quad_type = 2
+    std::string num_weight_file_;
+
+    // quad_weight_file_: File name of weights W(x)
+    // Set if weighted_inner = true
+    std::string quad_weight_file_;
+
+
+    //**** run settings - MAY need to be set (Model info) ****//
+
+    // Lower/upper interval of each parameter
+    // Set if load_from_file = false
+    double *params_low_, *params_high_;
+
+    // Number of samplings in the interval [param_low,param_high]
+    // Set if load_from_file = false.
+    int *params_num_;
+
+    // Set if load_from_file = true
+    std::string ts_file_name_;
+
+
+    //**** Internal data not set in the configuration file ****//
+
+    // True if ts file loaded, otherwise false. Useful to avoid termination
+    // when file not found. Useful when a basis validation study might not
+    // make this available
+    bool ts_file_exists_;
+
     int ts_size_;
 };
 
